3bb6496eb0f302ba6327551325a1963abfb6489a
bloke101 <izagladkin@mobiledep.ru>
2024-06-07 11:01:58 +0300

    Added a new problem subdomain. Solved new problems.

---
+++ src/Introduction/JavaStdinAndStdout.java
@@ -0,0 +1,19 @@
+package Introduction;
+
+import java.util.Scanner;
+
+public class JavaStdinAndStdout {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        scanner.close();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
---
+++ src/Introduction/WelcomeToJava.java
@@ -0,0 +1,8 @@
+package Introduction;
+
+public class WelcomeToJava {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
